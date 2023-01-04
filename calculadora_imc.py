f197e2b4979698e6d8b4d99707762b67a49013a7
tiagobeloh <tc-belo@hotmail.com>
2023-01-04 20:29:48 -0300

    meu primeiro commit

---
+++ calculadora_imc.py
@@ -0,0 +1,14 @@
+# calculo do IMC
+#
+altura = input('qual a sua altura ?')
+peso = input('qual o seu peso?')
+total = float(peso)/float(altura)
+if(total <= 18.4):
+  print('imc invalido')
+elif(total >= 18.5 and total <= 24.9):
+  print('normal')
+elif(total >= 25.0 and total <= 29.9):
+  print('sobrepeso')
+else:
+ print('obsidade')
+ print(total)
